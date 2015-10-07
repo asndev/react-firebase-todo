08348e394b92767680b6269bd08240a1db7ff354
stepan <stepan@ws-asn.patronas.int>
2015-10-07 15:13:12 +0200

    init project

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return <h2>Foobar.</h2>;
+  }
+});
+
+var element = React.createElement(App, {});
+
+React.render(element, document.querySelector('#mainApp'));
